d5261626e95fe71c844b4b7b163890e11b9848e2
Fernando Toledo <fernandohtr@gmail.com>
2023-02-09 17:54:20 -0300

    Initial commit

---
+++ src/test/java/com/fernandohtr/oauthsocial/OauthsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandohtr.oauthsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
