3820147e5918a45287f3edbc33df7a3d331856ef
Fernando Toledo <fernandohtr@gmail.com>
2023-02-12 10:25:46 -0300

    Get username when logging in

--- src/main/java/com/fernandohtr/oauthsocial/SocialLoginController.java
+++ src/main/java/com/fernandohtr/oauthsocial/SocialLoginController.java
@@ -1,15 +1,27 @@
 package com.fernandohtr.oauthsocial;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class SocialLoginController {
 
     @GetMapping
-    public String getHomePage() {
+    public String getHomePage(Authentication authentication, Model model) {
+
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = user.getAttributes();
+
+        String name = attributes.getOrDefault("localizedFirstName", attributes.get("given_name")).toString();
+
+        model.addAttribute("name", name);
         return "index";
     }
 
